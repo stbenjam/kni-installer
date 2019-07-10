8b6002def3b17391ac2fc7a2cb466c20ea5ad1f7
Stephen Benjamin <stephen@redhat.com>
2019-07-10 09:21:24 -0400

    pkg/asset/installconfig: remove baremetal platform survey
    It is not expected that users will setup the baremetal platform in an
    interactive way, given the number of data items we need from the user.
    They will need to supply this data as an install-config.yaml instead.
    
    This commit removes the survey code.

--- pkg/asset/installconfig/platform.go
+++ pkg/asset/installconfig/platform.go
@@ -5,19 +5,17 @@ import (
 	"sort"
 
 	"github.com/pkg/errors"
-	survey "gopkg.in/AlecAivazis/survey.v1"
+	"gopkg.in/AlecAivazis/survey.v1"
 
 	"github.com/openshift-metalkube/kni-installer/pkg/asset"
 	awsconfig "github.com/openshift-metalkube/kni-installer/pkg/asset/installconfig/aws"
 	azureconfig "github.com/openshift-metalkube/kni-installer/pkg/asset/installconfig/azure"
-	baremetalconfig "github.com/openshift-metalkube/kni-installer/pkg/asset/installconfig/baremetal"
 	libvirtconfig "github.com/openshift-metalkube/kni-installer/pkg/asset/installconfig/libvirt"
 	openstackconfig "github.com/openshift-metalkube/kni-installer/pkg/asset/installconfig/openstack"
 	vsphereconfig "github.com/openshift-metalkube/kni-installer/pkg/asset/installconfig/vsphere"
 	"github.com/openshift-metalkube/kni-installer/pkg/types"
 	"github.com/openshift-metalkube/kni-installer/pkg/types/aws"
 	"github.com/openshift-metalkube/kni-installer/pkg/types/azure"
-	"github.com/openshift-metalkube/kni-installer/pkg/types/baremetal"
 	"github.com/openshift-metalkube/kni-installer/pkg/types/libvirt"
 	"github.com/openshift-metalkube/kni-installer/pkg/types/none"
 	"github.com/openshift-metalkube/kni-installer/pkg/types/openstack"
@@ -69,11 +67,6 @@ func (a *platform) Generate(asset.Parents) error {
 		}
 	case vsphere.Name:
 		a.VSphere, err = vsphereconfig.Platform()
-		if err != nil {
-			return err
-		}
-	case baremetal.Name:
-		a.BareMetal, err = baremetalconfig.Platform()
 		if err != nil {
 			return err
 		}
